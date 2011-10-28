8f143ebdcb2cd7404959b4fc99256cbeced3b457
Arijit De <talktoari@gmail.com>
2011-10-28 13:55:16 +0530

    Initial Commit Arijit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vpapp::Application.config.secret_token = '63469062f94e4939f34db6be9ed3a4eb12a18b9b282a59a8fac59ea013f4afa9d5741bef7c35baa981e3f2e6aa789af3b96c5b3a4cf321e96e0b98f835bcc146'
