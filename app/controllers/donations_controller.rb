b1127967fe7fee22525abacfc67c30e4dd77fe01
Arijit De <talktoari@gmail.com>
2012-05-02 23:40:26 +0530

    fixed donation delete return path

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -92,7 +92,7 @@ class DonationsController < ApplicationController
     @donation.destroy
 
     respond_to do |format|
-      format.html { redirect_to donations_url }
+      format.html { redirect_to :back }
       format.json { head :ok }
     end
   end
